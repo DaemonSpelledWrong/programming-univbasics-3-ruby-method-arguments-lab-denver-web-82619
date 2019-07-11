da703c716ce0fb094a2658f3f343eefe0ca77a8a
Damon Self <damongself@gmail.com>
2019-07-11 10:24:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,10 +2,14 @@ def introduction(name)
   puts "Hi, my name is #{name}"
 end 
 
-def introduction_with_language
-  
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional
-  
+def introduction_with_language_optional(name, language)
+  if(language == false) do
+    puts "Hi, my name is #{name} and I am learning to program in Ruby"
+  end else do
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  end
 end 
